9c7fff20488760147e21a562c32f8d17f2deec08
castillocarlosr <castillocarlosr@gmail.com>
2018-12-07 17:49:58 -0800

    set up array I/O display list

--- CodeChallenges/CodeChallenges/Program.cs
+++ CodeChallenges/CodeChallenges/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,7 +22,7 @@ namespace CodeChallenges
         {
             Console.Clear();
             Console.WriteLine("Code Challenges Program.  Choose a number problem.........");
-            Console.WriteLine("1) Problem 1:Array Max Result");
+            Console.WriteLine("1) Problem 1: Array Max Result");
             Console.WriteLine("2) Problem 2: Leap Year Calculator");
             Console.WriteLine("3) Problem 3: Perfect Sequence");
             Console.WriteLine("4) Problem 4: Sum of Rows");
@@ -60,13 +61,40 @@ namespace CodeChallenges
         private static void ArrayMaxResult()
         {
             //Write in some code block here -love carlos
-            Console.WriteLine("Test code 1.  Yup, this is only a test");
-            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Problem 1: Array Max Result");
+            Console.WriteLine("Choose 5 numbers between 1-10");
+            ArrayList userList = new ArrayList();
+
+            Console.Write("Choose the first number:  ");
+            string result = Console.ReadLine();
+            userList.Add(result);
+            Console.Write("Choose the second number: ");
+            string result2 = Console.ReadLine();
+            userList.Add(result2);
+            Console.Write("Choose the third number:  ");
+            string result3 = Console.ReadLine();
+            userList.Add(result3);
+            Console.Write("Choose the forth number:  ");
+            string result4 = Console.ReadLine();
+            userList.Add(result4);
+            Console.Write("Choose the fifth number:  ");
+            string result5 = Console.ReadLine();
+            userList.Add(result5);
+
+            Console.WriteLine(userList[0]);
+            Console.WriteLine(userList[1]);
+            Console.WriteLine(userList[2]);
+            Console.WriteLine(userList[3]);
+            Console.WriteLine(userList[4]);
+
+            Console.ReadKey();
         }
 
         private static void LeapYearCalculator()
         {
             //Write in some code block here -love carlos
+            Console.Clear();
             Console.WriteLine("Test code 2.  Yup, this is only a test");
             Console.ReadLine();
         }
@@ -74,6 +102,7 @@ namespace CodeChallenges
         private static void PerfectSequence()
         {
             //Write in some code block here -love carlos
+            Console.Clear();
             Console.WriteLine("Test code 3.  Yup, this is only a test");
             Console.ReadLine();
         }
@@ -81,6 +110,7 @@ namespace CodeChallenges
         private static void SumofRows()
         {
             //Write in some code block here -love carlos
+            Console.Clear();
             Console.WriteLine("Test code 4.  Yup, this is only a test");
             Console.ReadLine();
         }
